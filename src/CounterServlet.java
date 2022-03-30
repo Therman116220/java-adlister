3c5e91a0c811a01dc4e2945ca17978d2dd209b30
Therman116220 <thermanhaynes@gmail.com>
2022-03-30 10:54:02 -0500

    update

--- src/CounterServlet.java
+++ src/CounterServlet.java
@@ -1,15 +1,23 @@
+import java.annotation.Webservlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRespose;
+import javax.servlet.http.HttpServletResponse;
 import java.IOExeption;
 
 @WebServelet(name = "CounterServlet", urlPatterns = "/counter")
 
 public class CounterServlet extends HttpServlet {
-    public int counter = 0;
 
-    protected void deGet(HttpServletRequest request, HttpServletResponse response) throwa IOException {
+
+    protected void deGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+       if(request.getParameter("reset") != null && request.getParemeter("reset").equalsIgnoreCase("reset")) {
+           counter = 0;
+       }
+
         counter++;
+
+
+        response.getWriter().println("<h1>The count currently is " + counter "on this load.</h1>");
 
 }
 
